b4673a7a78addae54f105035b83ee964fdddabc0
nihat417 <akreminihat@gmail.com>
2024-10-08 11:37:36 +0400

    Last Update Languages

--- src/screens/main/settingsScreen/languageScreen.jsx
+++ src/screens/main/settingsScreen/languageScreen.jsx
@@ -1,77 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { StyledScrollView,StyledView,StyledButton,GoBackSvg,StyledText, SearchSvg, StyledInput, StyledImage, AzerbaijanFlag, SelectedLangSvg, EnglanFlag, TrukeyFalg, RussiaFalg, SpainFlag } from '../../../common/StyledComponents';
+import React, { useState } from 'react';
+import { StyledScrollView, StyledView, StyledButton, GoBackSvg, StyledText, SearchSvg, StyledInput, AzerbaijanFlag, SelectedLangSvg, EnglanFlag, TrukeyFalg, RussiaFalg, SpainFlag } from '../../../common/StyledComponents';
+import { useNavigation } from '@react-navigation/native';
 
 const LanguageScreen = () => {
-    const [selected,setSelected]= useState(true);
-  return (
-    <StyledScrollView className='bg-[#161622] h-screen'>
+    const [selectedLanguage, setSelectedLanguage] = useState('1'); 
+    const navigation = useNavigation();
 
-        {/* header */}
+    const languages = [
+        { id: '1', name: 'Azerbaijan', flag: <AzerbaijanFlag /> },
+        { id: '2', name: 'English', flag: <EnglanFlag /> },
+        { id: '3', name: 'Turkish', flag: <TrukeyFalg /> },
+        { id: '4', name: 'Russian', flag: <RussiaFalg /> },
+        { id: '5', name: 'Spanish', flag: <SpainFlag /> },
+    ];
 
-        <StyledView className='flex-row justify-between mx-[20px] my-[40px] items-center'>
-            <StyledButton>
-                <GoBackSvg/>
-            </StyledButton>
-            <StyledText className='mr-[30px] text-white text-[18px]'>Language</StyledText>
-            <StyledView></StyledView>
-        </StyledView>
+    const handleSelect = (id) => {
+        setSelectedLanguage(id);
+    };
 
-        {/* search */}
-
-        <StyledView className='bg-[#232533] mx-[20px] rounded-[10px] flex-row items-center'>
-            <StyledView className='p-2'>
-                <SearchSvg />
+    return (
+        <StyledScrollView className='bg-[#161622] h-screen'>
+            {/* Header */}
+            <StyledView  className='relative mx-[20px] my-[40px] h-[50px]'>
+                <StyledButton onPress={()=>navigation.goBack()} className='absolute left-0 top-0 bottom-0 justify-center'>
+                    <GoBackSvg />
+                </StyledButton>
+                <StyledText className='text-white text-[18px] text-center'>
+                    Language
+                </StyledText>
             </StyledView>
-            <StyledInput placeholder='Search Language' placeholderTextColor='#BBBBC1' className='flex-1 text-white'/>
-        </StyledView>
 
-        {/* flags */}
-
-        <StyledView className={`flex-row items-center ${selected ? 'justify-between': '' } mx-[20px] mb-[10px] mt-[30px] p-[10px] border-b-[1px] border-[#232533]`}>
-            <StyledView className='flex-row items-center'>
-                <AzerbaijanFlag/>
-                <StyledText className='mx-[20px] text-[16px] text-[#fff]'>Azerbaijan</StyledText>
+            {/* Search */}
+            <StyledView className='bg-[#232533] mx-[20px] rounded-[10px] mb-[30px] flex-row items-center'>
+                <StyledView className='p-2'>
+                    <SearchSvg />
+                </StyledView>
+                <StyledInput 
+                    placeholder='Search Language' 
+                    placeholderTextColor='#BBBBC1' 
+                    className='flex-1 text-white'
+                />
             </StyledView>
-            {selected? <SelectedLangSvg/> : ''}
-        </StyledView>
-        
-        <StyledView className={`flex-row items-center ${selected ? 'justify-between': '' } mx-[20px] mb-[10px] p-[10px] border-b-[1px] border-[#232533]`}>
-            <StyledView className='flex-row items-center'>
-                <EnglanFlag/>
-                <StyledText className='mx-[20px] text-[16px] text-[#fff]'>English</StyledText>
-            </StyledView>
-            {selected? <SelectedLangSvg/> : ''}
-        </StyledView>
 
-        <StyledView className={`flex-row items-center ${selected ? 'justify-between': '' } mx-[20px] mb-[10px]  p-[10px] border-b-[1px] border-[#232533]`}>
-            <StyledView className='flex-row items-center'>
-                <TrukeyFalg/>
-                <StyledText className='mx-[20px] text-[16px] text-[#fff]'>Turkish</StyledText>
-            </StyledView>
-            {selected? <SelectedLangSvg/> : ''}
-        </StyledView>
-
-        <StyledView className={`flex-row items-center ${selected ? 'justify-between': '' } mx-[20px] mb-[10px] p-[10px] border-b-[1px] border-[#232533]`}>
-            <StyledView className='flex-row items-center'>
-                <RussiaFalg/>
-                <StyledText className='mx-[20px] text-[16px] text-[#fff]'>Russian</StyledText>
-            </StyledView>
-            {selected? <SelectedLangSvg/> : ''}
-        </StyledView>
-
-        <StyledView className={`flex-row items-center ${selected ? 'justify-between': '' } mx-[20px] mb-[10px] p-[10px] border-b-[1px] border-[#232533]`}>
-            <StyledView className='flex-row items-center'>
-                <SpainFlag/>
-                <StyledText className='mx-[20px] text-[16px] text-[#fff]'>Spainish</StyledText>
-            </StyledView>
-            {selected? <SelectedLangSvg/> : ''}
-        </StyledView>
-
-
-
-    </StyledScrollView>
-  )
-}
+            {/* Languages */}
+            {languages.map((lang) => (
+                <StyledButton 
+                    key={lang.id} 
+                    onPress={() => handleSelect(lang.id)} 
+                    className='flex-row items-center mx-[20px] mb-[10px] p-[10px] border-b-[1px] border-[#232533] justify-between'
+                >
+                    <StyledView className='flex-row items-center'>
+                        {lang.flag}
+                        <StyledText className='mx-[20px] text-[16px] text-[#fff]'>{lang.name}</StyledText>
+                    </StyledView>
+                    {selectedLanguage === lang.id && <SelectedLangSvg />}
+                </StyledButton>
+            ))}
+        </StyledScrollView>
+    );
+};
 
 export default LanguageScreen;
